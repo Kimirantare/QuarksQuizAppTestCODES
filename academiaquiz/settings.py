523172aa47604a5a749dd8418967c5d75c834ceb
Kimirantare <98355052+Kimirantare@users.noreply.github.com>
2022-02-02 13:07:01 +0200

    changes for heroku

--- academiaquiz/settings.py
+++ academiaquiz/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 import django.contrib.staticfiles
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'academiaquiz.urls'
@@ -126,6 +130,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -144,3 +150,6 @@ LOGIN_REDIRECT_URL = "web_profile"
 #     'rest_framework.permissions.IsAuthenticated',
 # ]
 # }
+
+
+django_heroku.settings(locals())
